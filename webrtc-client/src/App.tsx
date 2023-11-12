90688436249623d1a6b9094f7eabdef2562f3fcc
Atsushi <shimarisu.araiguma@gmail.com>
2023-11-12 10:23:54 +0900

    feat: Add websocket in webrtc-client

--- webrtc-client/src/App.tsx
+++ webrtc-client/src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import React, { useEffect, useRef } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const App: React.FC = () => {
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const peerConnection = new RTCPeerConnection();
+  const socket = io('http://localhost:5000'); // signaling-serverのURL
+
+  useEffect(() => {
+    // シグナリングサーバーからのメッセージを受信
+    socket.on('offer', async (offer: RTCSessionDescriptionInit) => {
+      try {
+        await peerConnection.setRemoteDescription(
+          new RTCSessionDescription(offer)
+        );
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+        socket.emit('answer', answer);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    // ICE候補の処理
+    peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        socket.emit('candidate', event.candidate);
+      }
+    };
+
+    // リモートストリームの処理
+    peerConnection.ontrack = (event) => {
+      if (localVideoRef.current) {
+        localVideoRef.current.srcObject = event.streams[0];
+      }
+    };
+
+    // シグナリングサーバーへの接続
+    socket.emit('join', 'client-room');
+
+    return () => {
+      socket.disconnect();
+      peerConnection.close();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>WebRTC Client</h1>
+      <video ref={localVideoRef} autoPlay playsInline />
     </div>
   );
-}
+};
 
 export default App;
